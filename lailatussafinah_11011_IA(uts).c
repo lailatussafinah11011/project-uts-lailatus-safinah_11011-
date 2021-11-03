a9afdcec1b06db200172e94452c053fd28338f95
lailatussafinah11011 <93624442+lailatussafinah11011@users.noreply.github.com>
2021-11-03 07:38:19 +0700

    Add files via upload

---
+++ lailatussafinah_11011_IA(uts).c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int a,b;
+
+printf ("----------------------------------------------------------------------\n");
+printf ("                        7 negara pemecah rekor \n");
+printf ("----------------------------------------------------------------------\n");
+    printf ("1. negara terkaya                              5. negara terkecil                                 \n");
+    printf ("2. nergara termiskin                           6. negara terluas                               \n");
+    printf ("3. negara tercanggih                           7. negara tertua                                \n");
+    printf ("4. negara terkotor                                       \n");
+  printf ("----------------------------------------------------------------------\n");
+    printf ("Masukkan Angka :");
+    scanf ("%d",&a);
+ printf ("----------------------------------------------------------------------\n");;
+    if (a ==2 ){
+
+        printf("Sudan Selatan\nmenjadi negara dengan tingkat kemiskinan tertinggi\n");
+        printf("di dunia pada 2021. Menurut lansiran worldpopulationreview.com bank\n");
+        printf("dunia mencatat rasio penduduk miskin dibanding populasi di Sudan \n");
+        printf("Selatan mencapai 82,3%.n");
+        printf("\n");
+    }
+    printf ("----------------------------------------------------------------------\n");
+     if (a ==1 ){
+
+        printf("Luksemburg\ndidaulat sebagai negara terkaya di dunia pada 2021\n");
+        printf("yang dinilai berdasarkan produk domestik bruto (PDB) atas dasar\n");
+        printf("paritas daya beli atau purchasing Power Parity (PPP) per kapita\n");
+        printf("negara ini yang sebesar US$ 110.001 atau setara Rp 1,69 miliar\n");
+        printf("(kurs Rp 14.320)\n");
+    printf ("----------------------------------------------------------------------\n");
+    }
+
+if (a ==3 ){
+
+        printf("Jepang\nNegara Jepang memang sudah terkenal dengan negara inovasi teknologi\n");
+        printf("canggihnya. Jepang menjadi negara teknologi termaju yang sangat istimewa\n");
+        printf("di berbagai bidang mulai robotik,elektronik,mesin hingga robotika\n");
+
+    printf ("----------------------------------------------------------------------\n");
+    }
+
+
+     if (a ==4 ){
+
+        printf("Bangladesh\nNegara terkotor di dunia pertama dilihat dari tingkat \n");
+        printf("polusinya adalah Bangladesh \n");
+    printf ("----------------------------------------------------------------------\n");
+    }
+
+
+
+    if (a ==5 ){
+
+        printf("Vatikan\nNegara terkecil pertama yaotu Vatikan. Luas negara ini hanya \n");
+        printf("mencapai 0,004 kilometer persegi saja\n");
+    printf ("----------------------------------------------------------------------\n");
+    }
+
+    if (a ==6 ){
+
+        printf("Rusia\nnegara yang membentang luas di sebelah utara Rusia dan timur\n");
+        printf("eropa menempati posisi pertama sebagai negara terbesar di dunia\n");
+        printf("bedasarkan luasnya dengan 17,1juta kilometer\n");
+    printf ("----------------------------------------------------------------------\n");
+    }
+
+    if (a ==7 ){
+
+        printf("Mesir\nMenurut sejarah, Mesir menjadi negara tertua di dunia. Dinasti mesir\n");
+        printf("mesir pertama berdiri sekitar 3100 SM.\n");
+
+    printf ("----------------------------------------------------------------------\n");
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+    }
